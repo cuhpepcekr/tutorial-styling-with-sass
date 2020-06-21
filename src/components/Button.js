a450ecd1acc2a7568a3136427fc902b6acd004a2
cuhpepcekr <lintogm@gmail.com>
2020-06-21 18:11:54 +0900

    Sass 버튼 A - size props 설정하기

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,14 @@
 import React from "react";
+import classNames from "classnames";
 import "./Button.scss";
 
 // size : large, medium, small
 function Button({ children, size }) {
-  return <button className={["Button", size].join(" ")}>{children}</button>;
+  return <button className={classNames("Button", size)}>{children}</button>;
 }
+
+Button.defaultProps = {
+  size: "medium",
+};
 
 export default Button;
