70c91e11e2c96d719786f335988246c05c2880d2
cuhpepcekr <lintogm@gmail.com>
2020-06-21 17:57:12 +0900

    yarn add classnames

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Button.scss";
 
-function Button({ children }) {
-  return <button className="Button">{children}</button>;
+// size : large, medium, small
+function Button({ children, size }) {
+  return <button className={["Button", size].join(" ")}>{children}</button>;
 }
 
 export default Button;
